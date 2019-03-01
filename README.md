3c8bbd29796348bd99316df125c237cff1edea18
Erin Hancock <welljer@gmail.com>
2019-02-28 20:57:52 -0700

    iSaunteredOverHereToMeetYou

---
+++ README.md
@@ -0,0 +1,13 @@
+# Salt City Paws
+
+
+
+## Technologies Used
+
+* [Node](https://nodejs.org/en/doc)
+* [Mysql](https://dev.mysql.com/doc/)
+
+### Packages Used
+
+* [Google Calendar](https://github.com/yuhong90/node-google-calendar)
+* []
--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,11 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE saunterer_db;
+USE saunterer_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE saunterer
+(
+        id INT NOT NULL AUTO_INCREMENT,
+        client_name VARCHAR(120) NOT NULL,
+        client_phone VARCHAR(20) NOT NULL,
+        client_email VARCHAR(120) NOT NULL,
+        PRIMARY KEY (id)
+)
