bd4cff12e3b004ec5e9d8e6c30da2dcc44b148da
Erin Hancock <welljer@gmail.com>
2019-03-01 20:26:57 -0700

    iSaunteredOverToYourDB

--- models/schema.sql
+++ models/schema.sql
@@ -7,5 +7,9 @@ CREATE TABLE saunterer
         client_name VARCHAR(120) NOT NULL,
         client_phone VARCHAR(20) NOT NULL,
         client_email VARCHAR(120) NOT NULL,
+        home_address VARCHAR(60) NOT NULL,
+        dogs_name VARCHAR(20) NOT NULL,
+        dog_breed VARCHAR(20) NOT NULL,
+        dog_age VARCHAR(20) NOT NULL,
         PRIMARY KEY (id)
 )
