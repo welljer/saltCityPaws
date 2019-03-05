c1181b42f30986a38707261f1ab40d4624739ee4
Erin Hancock <welljer@gmail.com>
2019-03-04 20:28:25 -0700

    theSevenDwarvesWistledUntilTheirAxesBroke

--- models/human.js
+++ models/human.js
@@ -1,34 +1,21 @@
-'use strict';
+// ----------------- this be our Human DB Model ------------------------
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('human', [{
-      human_name: 'Floyd Council',
-      human_phone: '(202) 224-5444',
-      human_email: 'theFloydinPink@peidmontBlues.com',
-      }, {
-      human_name: 'Syd Barret',
-      human_phone: '8015381029',
-      human_email: 'theMadCapLaughs@wetherby.com',
-    }]);
-  
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+"use strict";
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
-};
+module.exports = function(sequelize, DataTypes){
+  var Human = sequelize.define('Dog',{
+    human_name:  {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    human_phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    human_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+   return Human;
+  };
